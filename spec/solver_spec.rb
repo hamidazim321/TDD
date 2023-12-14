a2a012d7c4316356c83457f92672cc789cbda6c0
hamid321 <hamidazim321@gmail.com>
2023-12-14 13:22:35 +0500

    Add template files

---
+++ spec/solver_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative 'solver'
+
+describe Solver do
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  context 'Initialize Solver' do
+    it 'Test for factorial method here' do
+    end
+
+    it 'Test for factorial method here' do
+    end
+
+    it 'Test for factorial method here' do
+    end
+  end
+end
