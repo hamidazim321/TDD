1c68dcdfe59bab18df4bc6ac40766c9afe30a9de
hamid321 <hamidazim321@gmail.com>
2023-12-14 13:48:11 +0500

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,5 +1,17 @@
 class Solver
-  def factorial; end
+  def factorial(n)
+    if n == 0
+      return 1
+    elsif n < 0
+      raise ArgumentError
+    else
+      result = 1
+      (1..n).each do |num|
+        result *= num
+      end
+      return result
+    end
+  end
 
   def reverse; end
 
